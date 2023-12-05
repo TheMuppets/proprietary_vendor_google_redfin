eb35c4eadc60eb6b1fcfb302fc2380d0db41f4f2
Michael Bestas <mkbestas@lineageos.org>
2023-12-05 21:37:45 +0200

    redfin: Import firmware from UP1A.231105.001
    Change-Id: I65feb4af440809abe16025762e78ed7acd3ae681

--- Android.mk
+++ Android.mk
@@ -6,4 +6,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),redfin)
 
+$(call add-radio-file,radio/abl.img)
+$(call add-radio-file,radio/aop.img)
+$(call add-radio-file,radio/devcfg.img)
+$(call add-radio-file,radio/featenabler.img)
+$(call add-radio-file,radio/hyp.img)
+$(call add-radio-file,radio/keymaster.img)
+$(call add-radio-file,radio/modem.img)
+$(call add-radio-file,radio/qupfw.img)
+$(call add-radio-file,radio/tz.img)
+$(call add-radio-file,radio/uefisecapp.img)
+$(call add-radio-file,radio/xbl.img)
+$(call add-radio-file,radio/xbl_config.img)
+
 endif
