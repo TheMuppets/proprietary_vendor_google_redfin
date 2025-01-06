d4ade32850b5d77c076e84b2bc09de1cd1b0c1d2
Michael Bestas <mkbestas@lineageos.org>
2025-01-06 23:14:15 +0200

    redfin: Remove OMX blobs
    Change-Id: I2a83a9d00b334a57847f49ec3d9a00f70d2ae61e

--- redfin-vendor.mk
+++ redfin-vendor.mk
@@ -1099,30 +1099,12 @@ PRODUCT_PACKAGES += \
     lib-rtpsl \
     lib-siputility \
     lib-uceservice \
-    libAlacSwDec \
-    libApeSwDec \
     libC2D2 \
     libCB \
-    libFlacSwDec \
     libGPQTEEC_vendor \
     libGPTEE_vendor \
     libGPreqcancel \
     libGPreqcancel_svc \
-    libI420colorconvert \
-    libMpeg4SwEncoder \
-    libMpeghSwEnc \
-    libOmxAacDec \
-    libOmxAlacDec \
-    libOmxAlacDecSw \
-    libOmxAmrDec \
-    libOmxAmrwbplusDec \
-    libOmxApeDec \
-    libOmxApeDecSw \
-    libOmxEvrcDec \
-    libOmxG711Dec \
-    libOmxMpeghEncSw \
-    libOmxQcelp13Dec \
-    libOmxWmaDec \
     libOpenCL-pixel \
     libOpenCL \
     libQSEEComAPI \
@@ -1457,13 +1439,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspmhal@1.0-impl \
     vendor.qti.qspmhal@1.0 \
     vendor.qti.voiceprint@1.0 \
-    libOmxDsdDec \
-    libOmxMpeghDecSw \
     libadm \
     libadpcmdec \
     libcppf \
     libdrc \
-    libdsd2pcm \
     libfastcrc \
     libmmosal \
     libsmwrapper \
