c1d121bc02593072b2f441afab86cc09929a4bbc
Michael Bestas <mkbestas@lineageos.org>
2025-12-31 03:04:28 +0200

    redfin: Use libprotobuf-cpp-{lite,full}-21.7 from hardware/lineage/compat
    Change-Id: I07eb8e11057f254bfdaaf3af128d526c9d127ae1

--- redfin-vendor.mk
+++ redfin-vendor.mk
@@ -1243,8 +1243,6 @@ PRODUCT_PACKAGES += \
     libperipheral_client \
     libpowercallback \
     libpowercore \
-    libprotobuf-cpp-full-21.7 \
-    libprotobuf-cpp-lite-21.7 \
     libpvr \
     libqcbor \
     libqcc_file_agent \
